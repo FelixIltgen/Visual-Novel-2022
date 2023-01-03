7f202d2b47c1fbb040c39a61884f889a0a1eaecf
Felix iltgen <felix.horst.iltgen@hs-furtwangen.de>
2023-01-03 14:25:09 +0100

    Add text in first scene

--- Template/Source/Scenes/firstScene.ts
+++ Template/Source/Scenes/firstScene.ts
@@ -5,16 +5,18 @@ namespace Template {
         
         let text = {
             narrator: {
-                T0001: "Hallo"
+                T0001: "Valentin macht sich auf den Weg zum Bergwerk."
             },
             ben: {
-                T001: ""
+                T001: "Tschüss Papa."
             },
             maria: {
-
+                T0001: "Alles klar, bis später pass auf dich auf, heute ist doch ein wichtiger Tag."
             },
             valentin: {
-
+                T0001: "Okay, ich mache mich fertig und mache mich auf den Weg.",
+                T0002: "Natürlich, ich passe immer auf mich auf, gerade bei so einem wichtigen Tag.",
+                T0003: "Tschüss, ihr beiden, habt einen schönen Tag."
             }
         };
     
