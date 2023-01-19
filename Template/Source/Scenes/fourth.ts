f4881e2de92a507e61eedf3412e24981f5a20755
Felix iltgen <felix.horst.iltgen@hs-furtwangen.de>
2023-01-19 17:42:04 +0100

    fourth Scene

--- Template/Source/Scenes/fourth.ts
+++ Template/Source/Scenes/fourth.ts
@@ -10,16 +10,29 @@ namespace myNovel {
                 T0004: "Alles klar...",
                 T0005: "...",
                 T0006: "Okay, das Dynamit ist platziert, lass uns in Deckung gehen.",
-                T0007: "Bist du bereit?"
+                T0007: "Bist du bereit?",
+                T0008: "",
+                T0009: "",
+                T00010: "",
+                T00011: ""
             },
             Andrea: {
                 T0001: "Okay, lass uns loslegen.",
                 T0002: "Lass mich das Geröll wegräumen, dann kannst du dich um die letzte Sprengladung kümmern und das Dynamit holen.",
                 T0003: "Ja, von meiner Seite ist alles bereit, du kannst das Dynamit platzieren.",
-                T0004: "Ja, ich bin bereit ... lass es knallen!!"
+                T0004: "Ja, ich bin bereit ... lass es knallen!!",
+                T0005: "",
+                T0006: "",
+                T0007: "",
+                T0008: ""
             },
             Narrator: {
-                T0001: "Valentin geht zum Lager und holt die letzte Stange Dynamit."
+                T0001: "Valentin geht zum Lager und holt die letzte Stange Dynamit.",
+                T0002: "",
+                T0003: "",
+                T0004: "",
+                T0005: "",
+                T0006: ""
             }
         };
         await ƒS.Location.show(locations.mineShaftTwo);
